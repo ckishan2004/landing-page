97e3c40afe8c13d5635e94b62d95aacc910f724a
ckishan2004 <chavdakishan292@gmail.com>
2025-02-26 16:19:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const reserveButton = document.querySelector(".btn");
+
+    reserveButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        alert("Reservation feature coming soon!");
+    });
+});
